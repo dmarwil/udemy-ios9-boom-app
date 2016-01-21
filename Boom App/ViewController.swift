9e42b0567735fe10fba62d8523e4a3f7cf81e603
David Marwil <punkisgreat@gmail.com>
2016-01-21 09:06:13 -0700

    Initial Commit

---
+++ Boom App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boom App
+//
+//  Created by David Marwil on 1/21/16.
+//  Copyright © 2016 David Marwil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
