3b8b0d06de75495b9f0de92759e34b2b12f6f2f7
David Marwil <punkisgreat@gmail.com>
2016-03-28 16:21:42 -0600

    first commit with command line

--- Boom App/ViewController.swift
+++ Boom App/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    @IBOutlet weak var chakraGirl: UIImageView!
+    @IBOutlet weak var hideChakraGirlButton: UIButton!
+    
+    var imageSwitcher = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func superCoolTheApp(sender: AnyObject) {
+        backgroundImage.hidden = false
+        uncoolButton.hidden = true
+    }
+    @IBAction func hideOrShowChakraGirl(sender: AnyObject) {
+        if imageSwitcher == 0 {
+            chakraGirl.hidden = true
+            hideChakraGirlButton.titleLabel!.text = "Show Chakra Girl"
+            imageSwitcher = 1;
+        }
+        else if imageSwitcher == 1 {
+            chakraGirl.hidden = false
+            hideChakraGirlButton.titleLabel!.text = "Hide Chakra Girl"
+            imageSwitcher = 0
+        }
+    }
 
 }
 
